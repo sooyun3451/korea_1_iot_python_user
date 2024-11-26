50cdb40905b14c1dd8ab00f09a4a46ca31dd3419
sooyun3451 <sooyun020820@gmail.com>
2024-11-26 12:01:05 +0900

    20241126

--- src/main.py
+++ src/main.py
@@ -1,11 +1,29 @@
 from src.entity.user_entity import User
-from src.repository.user_repository import UserRepositoryStudy
+from src.view.signup import SigUpView
 
 def main():
-  newUser = User(username='ddd', password='ddd123', name='홍길북', email='ddd@gmail.com')
-  userRepository = UserRepositoryStudy()
-  userRepository.insert(newUser)
-  userRepository.findById(4)
+    signupView = SigUpView()
+    while True:
+        print("""
+    1. 회원가입
+    2. 사용자 조회
+    3. 사용자 전체 조회
+    4. 사용자 삭제 
+    q. 프로그램 종료
+    """)
+        selectedMenu = input("선택 >>>")
+        if selectedMenu in ("q", "Q"):
+            print("프로그램을 종료합니다.")
+            return
 
-if __name__ == "__main__": 
-  main()
+        if selectedMenu == "1":
+            signupView.showSignup()
+        elif selectedMenu == "2":
+            pass
+        elif selectedMenu == "3":
+            pass
+        elif selectedMenu == "4":
+            pass
+
+if __name__ == "__main__":
+    main()
--- src/repository/user_repository.py
+++ src/repository/user_repository.py
@@ -1,30 +1,62 @@
-from pymysql import cursors
+from tkinter.constants import E
+
+from pymysql import cursors, connect
 from dataclasses import dataclass
 
 from src.config.database_config import DatabaseConfig
 from src.entity.user_entity import User
 
-@dataclass
-class UserRepositoryStudy:
+class UserRepository:
 
-  def insert(self, user: User):
-    connection = DatabaseConfig.getConnection()
-    cursor = connection.cursor()
-    sql = '''
-    insert into user_tb
-    values(default, %s, %s, %s, %s, now(), now())
-    '''
-    insertCount = cursor.execute(query=sql, args=(user.username, user.password, user.name, user.email))
-    print(f'User데이터 추가 성공 {insertCount}건')
-    connection.commit()
+  @staticmethod
+  def findByUsername(username: str):
+    foundUser = None
 
+    try:
+      connection = DatabaseConfig.getConnection()
+      try:
+        cursor = connection.cursor(cursor=cursors.DictCursor)
+        sql = "select * from user_tb where username = %s"
+        cursor.execute(query=sql, args=(username, ))
+        result = cursor.fetchone()
+        if bool(result):
+          foundUser = User(
+            userId=result['user_id'],
+            username=result['username'],
+            password=result['password'],
+            name=result['name'],
+            email=result['email'],
+            createDate=result['create_date'],
+            updateDate=result['update_date']
+          )
+      except Exception as e:
+        print(e)
+      finally:
+        connection.close()
+    except Exception as e:
+      print(e)
+      print("데이터베이스 연결 실패")
 
-  def findById(self, user_id: int):
-    connection = DatabaseConfig.getConnection()
-    cursor = connection.cursor(cursor=cursors.DictCursor)
-    sql = '''
-    select * from user_tb where user_id = %s
-    '''
-    cursor.execute(query=sql, args=(user_id,))
-    result = cursor.fetchone()
-    print(result)
+    return foundUser
+
+  @staticmethod
+  def saveUser(user: User):
+    connection = None
+    try:
+      connection = DatabaseConfig.getConnection()
+      try:
+        cursor = connection.cursor()
+        sql = '''
+          insert into user_tb values(default, %s, %s, %s, %s, now(), now())
+        '''
+        cursor.execute(query=sql, args=(user.username, user.password, user.name, user.email))
+        user.userId = cursor.lastrowid
+        # lastrowid: 마지막 로우의 id, 마지막에 들어간 데이터의 PK 값
+        connection.commit()
+      except Exception as e:
+        print(e)
+        print("사용자 정보 추가 실패!!!")
+      finally:
+        connection.close()
+    except Exception as e:
+      print(e)
